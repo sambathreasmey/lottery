5ba4c1811252b2e982e401ea69520f928fcc65de
sambathreasmey <reasmeysambath@gmail.com>
2025-01-26 16:41:51 +0700

    enhance encryption request

--- controllers/system/userController.js
+++ controllers/system/userController.js
@@ -43,28 +43,33 @@ function deriveKey(password, salt) {
     return CryptoJS.PBKDF2(password, salt, { keySize: 256 / 32, iterations: 100 });
 }
 
-// Function to decrypt data
+// Function to decrypt data with expiration check
 function decrypt(encryptedData, password) {
-    // Extract salt, iv, and encrypted data
+    // Extract salt, iv, expiration time, and encrypted data
     const saltHex = encryptedData.substr(0, 32); // First 32 characters for salt
     const ivHex = encryptedData.substr(32, 32); // Next 32 characters for IV
-    const encryptedHex = encryptedData.substr(64); // Remaining characters for encrypted data
+    const expirationTimeHex = encryptedData.substr(64, 10); // Next 10 characters for expiration time
+    const encryptedHex = encryptedData.substr(74); // Remaining characters for encrypted data
 
     // Convert hex to WordArray
     const salt = CryptoJS.enc.Hex.parse(saltHex);
     const iv = CryptoJS.enc.Hex.parse(ivHex);
+    const expirationTime = parseInt(expirationTimeHex, 10); // Convert expiration time to integer
     const encrypted = CryptoJS.enc.Base64.parse(encryptedHex); // Encrypted data is in Base64
 
+    // Check if the current time exceeds the expiration time
+    const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
+    if (currentTime > expirationTime) {
+        return "expired";
+    }
     // Derive the key using PBKDF2
     const key = deriveKey(password, salt);
-
     // Decrypt the data
     const decrypted = CryptoJS.AES.decrypt(
         { ciphertext: encrypted },
         key,
         { iv: iv }
     );
-
     return decrypted.toString(CryptoJS.enc.Utf8); // Return decrypted string
 }
 
@@ -76,6 +81,10 @@ const loginUser = asyncHandler(async (req, res) => {
     const { encryptedData } = req.body;
 
     const jsonString = decrypt(encryptedData, process.env.AES_PASSWORD);
+    if (jsonString == "expired") {
+        res.status(400);
+        throw new Error("The session has expired!");
+    }
     const jsonObject = JSON.parse(jsonString);
 
     const { email, password } = jsonObject;
